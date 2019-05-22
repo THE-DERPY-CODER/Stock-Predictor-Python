938379138dd8efe2142e3828a692ea7f3d19cec6
THE-DERPY-CODER <50914873+THE-DERPY-CODER@users.noreply.github.com>
2019-05-22 08:51:16 -0400

    Update autocomplete_ticker_name.py

--- Data/AutoComplete/autocomplete_ticker_name.py
+++ Data/AutoComplete/autocomplete_ticker_name.py
@@ -6,14 +6,14 @@ import json
 
 # print("Beginning script for autocomplete")
 
-stocks = requests.get("http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(sys.argv[1])).json()['ResultSet']['Result']
+stocks = requests.get("http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(sys.argv[1])).json()['ResultSet']['Result'] # getting data and sorting through dictoinary
 
-data = []
+data = [] #the final data values to put in json
 
 for stock in stocks:
-    data.append(TickerData.Ticker(stock["symbol"]).info)
+    data.append(TickerData.Ticker(stock["symbol"]).info) # adding quote of the current stock
 
 with open('/Users/ozanmirza/desktop/Stock-Predictor-Python/Data/AutoComplete/autocomplete.json', 'w') as outfile:
-    json.dump(data, outfile, indent=4, sort_keys=True)
+    json.dump(data, outfile, indent=4, sort_keys=True) # saving the json and formatting it
 
 # print("Successfully autocompleted data for {}".format(sys.argv[1]))
